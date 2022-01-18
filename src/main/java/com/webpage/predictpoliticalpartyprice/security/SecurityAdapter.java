2c0e57399afb4f386a5688ea47cb19f455b4aa2a
lseeger <lseeger@inovex.de>
2022-01-18 20:55:05 +0100

    added security adapter

---
+++ src/main/java/com/webpage/predictpoliticalpartyprice/security/SecurityAdapter.java
@@ -0,0 +1,76 @@
+package com.webpage.predictpoliticalpartyprice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+
+
+@Configuration
+@EnableWebSecurity
+@ConditionalOnWebApplication
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private DataSource dataSource;
+//
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new CustomUserDetailsImpl();
+//    }
+//
+//    /**
+//     * Creates an authentication provider using bcrypt as password encoder and the custom user details service
+//     * @return new instance of CustomUserDetailsImpl
+//     */
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService());
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//
+//        return authProvider;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)  {
+//        auth.authenticationProvider(authenticationProvider());
+//    }
+
+    /**
+     * Configures security options
+     * @param http Object to be configured
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable().authorizeRequests()
+                .antMatchers("/KanueleData/**").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("userName")
+                .defaultSuccessUrl("/KanueleData/selectfeatures")
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll();
+    }
+}
