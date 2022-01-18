ade41e05b113bd54c7465d8ea8c9315ee4f12a0d
lseeger <lseeger@inovex.de>
2022-01-18 20:52:13 +0100

    added context loading tests

--- src/test/java/com/webpage/predictpoliticalpartyprice/PredictPoliticalPartyPriceApplicationTests.java
+++ src/test/java/com/webpage/predictpoliticalpartyprice/PredictPoliticalPartyPriceApplicationTests.java
@@ -1,13 +1,45 @@
 package com.webpage.predictpoliticalpartyprice;
 
+import com.webpage.predictpoliticalpartyprice.dao.ContractLogDao;
+import com.webpage.predictpoliticalpartyprice.mapper.ContractLogRowMapper;
+import com.webpage.predictpoliticalpartyprice.services.ContractLogService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class PredictPoliticalPartyPriceApplicationTests {
+    @Resource
+    @Qualifier("week")
+    ContractLogService contractLogWeekService;
+
+    @Resource
+    @Qualifier("day")
+    ContractLogService contractLogDayService;
+
+    @Resource
+    ContractLogRowMapper contractLogRowMapper;
+
+    @Resource
+    @Qualifier("weekdao")
+    ContractLogDao contractLogDaoWeek;
+
+    @Resource
+    @Qualifier("daydao")
+    ContractLogDao contractLogDaoDay;
+
 
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(contractLogDaoDay);
+        Assertions.assertNotNull(contractLogDaoWeek);
+        Assertions.assertNotNull(contractLogRowMapper);
+        Assertions.assertNotNull(contractLogDayService);
+        Assertions.assertNotNull(contractLogWeekService);
     }
 
 }
