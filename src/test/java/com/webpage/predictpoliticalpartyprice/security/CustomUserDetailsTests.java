e1ad61c76a190a13230083279df290e58ec1d997
lseeger <lseeger@inovex.de>
2022-01-30 20:31:46 +0100

    added custom user details tests

---
+++ src/test/java/com/webpage/predictpoliticalpartyprice/security/CustomUserDetailsTests.java
@@ -0,0 +1,24 @@
+package com.webpage.predictpoliticalpartyprice.security;
+import static org.junit.Assert.*;
+import com.webpage.predictpoliticalpartyprice.entities.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class CustomUserDetailsTests {
+
+    @Test
+    public void givenUserWithPasswordName_whenCustomUserDetailsCreatedFromUser_thenGivenAuthoritiesShouldReturnRoleUserPasswordAndName(){
+        User user = new User();
+        user.setUsername("testuser");
+        user.setEmail("test@gmail.com");
+        user.setPassword("somepassword");
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+        SimpleGrantedAuthority exspectedAuthority = new SimpleGrantedAuthority("ROLE_USER");
+
+        assert(exspectedAuthority.getAuthority().equals("ROLE_USER"));
+        assert(customUserDetails.getUsername().equals("testuser"));
+        assert(customUserDetails.getPassword().equals("somepassword"));
+    }
+}
